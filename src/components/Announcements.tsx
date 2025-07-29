01ee325a60a48450c3ee5dce12c003b415c85c64
Kamil Ulupinar <kamil.ulupinar@gmail.com>
2025-07-29 11:06:29 +0300

    Add Announcements component and integrate it into AdminPage; update EventCalendar layout and styles
    Completed admin page.

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,40 @@
+const Announcements = () => {
+  return (
+    <div className='bg-white p-4 rounded-md'>
+        <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold">Duyurular</h1>
+            <span className="text-xs text-gray-400">Tümünü Gör</span>
+        </div>
+        <div className="flex flex-col gap-4 mt-4">
+            <div className="bg-KmlSkyLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit </h2>
+                    <span className="text-xs text-gray-500 bg-white rounded-md px-1 py-1">29.07.2025
+                    </span>
+                </div>
+                <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+            </div>
+            <div className="bg-KmlPurpleLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit </h2>
+                    <span className="text-xs text-gray-500 bg-white rounded-md px-1 py-1">29.07.2025
+                    </span>
+                </div>
+                     <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+
+            </div>
+            <div className="bg-KmlYellowLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit </h2>
+                    <span className="text-xs text-gray-500 bg-white rounded-md px-1 py-1">29.07.2025
+                    </span>
+                </div>
+                     <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Announcements
