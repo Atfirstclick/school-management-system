26e6c7aa223007b23172c4d3a599798092275ff3
Kamil Ulupinar <kamil.ulupinar@gmail.com>
2025-08-03 19:32:08 +0300

    Add SingleStudentPage component with user info card, performance chart, and navigation links

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -4,7 +4,7 @@ import Performance from "@/components/Performance"
 import Image from "next/image"
 import Link from "next/link"
 
-const SingleTeacherPage = () => {
+const SingleStudentPage = () => {
   return (
     <div className='flex-1 p-4 flex flex-col gap-4 xl:flex-row'>
         {/* LEFT */}
@@ -16,7 +16,7 @@ const SingleTeacherPage = () => {
 <div className="bg-KmlSky py-6 px-4 rounded-md flex-1 flex gap-4">
   <div className="w-1/3 flex items-center">
     <Image 
-      src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200" 
+      src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200" 
       alt="" 
       width={144} 
       height={144} 
@@ -24,7 +24,7 @@ const SingleTeacherPage = () => {
     />
   </div>
   <div className="w-2/3 flex flex-col justify-between gap-4">
-    <h1 className="text-xl font-semibold">Hakan Belen</h1>
+    <h1 className="text-xl font-semibold">Sevgi Yapar</h1>
     <p className="text-sm text-gray-500 break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
         <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
@@ -59,22 +59,22 @@ const SingleTeacherPage = () => {
           <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
             <Image src="/singleBranch.png" alt='' width={24} height={24} className="w-6 h-6"/>
          <div className="">
-          <h1 text-xl font-semibold >2</h1>
-          <span className="text-sm text-gray-400">Şube</span>
+          <h1 text-xl font-semibold >9.</h1>
+          <span className="text-sm text-gray-400">Sınıf</span>
          </div>
          </div>
           {/* CARD */}
           <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
           <Image src="/singleLesson.png" alt='' width={24} height={24} className="w-6 h-6"/>
          <div className="">
-          <h1 text-xl font-semibold >6</h1>
-          <span className="text-sm text-gray-400">Aylık Ders</span>
+          <h1 text-xl font-semibold >8</h1>
+          <span className="text-sm text-gray-400">Ders</span>
          </div>
          </div>
         {/* CARD */}
           <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">            <Image src="/singleClass.png" alt='' width={24} height={24} className="w-6 h-6"/>
          <div className="">
-          <h1 text-xl font-semibold >6</h1>
+          <h1 text-xl font-semibold >9-C</h1>
           <span className="text-sm text-gray-400">Sınıf</span>
          </div>
          </div>
@@ -82,7 +82,7 @@ const SingleTeacherPage = () => {
         </div>
         {/* BOTTOM */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-          <h1>Öğretmen Programı</h1>
+          <h1>Öğrenci Programı</h1>
           <BigCalendar/>
           </div>
         </div>
@@ -91,11 +91,12 @@ const SingleTeacherPage = () => {
         <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Kısayollar</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-          <Link className="p-3 rounded-md bg-KmlSkyLight" href="/">Sınıflar</Link>
-          <Link className="p-3 rounded-md bg-KmlPurpleLight" href="/">Dersler</Link>
-          <Link className="p-3 rounded-md bg-KmlYellowLight" href="/">Öğrenciler</Link>
+          <Link className="p-3 rounded-md bg-KmlSkyLight" href="/">Dersler</Link>
+          <Link className="p-3 rounded-md bg-KmlPurpleLight" href="/">Öğretmenler</Link>
           <Link className="p-3 rounded-md bg-pink-50" href="/">Testler</Link>
           <Link className="p-3 rounded-md bg-KmlSkyLight" href="/">Ödevler</Link>
+          <Link className="p-3 rounded-md bg-KmlYellowLight" href="/">Sonuçlar</Link>
+
           </div>
         </div>
         <Performance/>
@@ -105,4 +106,4 @@ const SingleTeacherPage = () => {
   )
 }
 
-export default SingleTeacherPage
+export default SingleStudentPage
